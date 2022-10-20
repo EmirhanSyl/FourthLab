91aec75268b0c65de160e8da8c5e9a748b2e10e6
EmirhanSyl <61618968+EmirhanSyl@users.noreply.github.com>
2022-10-20 16:55:27 +0300

    FirstPart

--- src/main/java/com/blackflower/fourthlab/FourthLab.java
+++ src/main/java/com/blackflower/fourthlab/FourthLab.java
@@ -6,16 +6,18 @@ public class FourthLab {
     public static void main(String[] args) {
         
         //----------------------------------------------------
-        System.out.println("");
+        PrintEvenNumbers(0);
         
         //----------------------------------------------------
-        System.out.println("");
+        MultipliesOfSeven(0);
         
         //----------------------------------------------------
-        System.out.println("");
+        int number = 5;
+        System.out.println("Fuctorial is" + number + " is: " + FindTheFuctorial(number));
         
         //----------------------------------------------------
-        System.out.println("");
+        int limit = 3000;
+        FindIterationCount(3000);
         
         //----------------------------------------------------
         System.out.println("");
@@ -30,20 +32,52 @@ public class FourthLab {
     
     
     
-    public static void Example_1(){
-        
+    public static void PrintEvenNumbers(int firstNum){
+        System.out.println("Even Numbers: ");
+        while(firstNum < 20){
+            firstNum += 2;
+            System.out.println(firstNum);
+        }
     }
     
-    public static void Example_2(){
+    public static void MultipliesOfSeven(int firstNum){
+        System.out.println("Multiplies of seven: ");
+        while(firstNum < 200){
+            firstNum += 7;
+            System.out.println(firstNum);
+        }
         
+        /**int i = 0;
+        while(i < 200){
+            if (i % 7 == 0) {
+                System.out.println(firstNum);
+            }
+            i++;
+        }**/
     }
     
-    public static void Example_3(){
+    public static int FindTheFuctorial(int number){
+        int i = 1;
+        int result = 1;
         
+        while(i <= number){
+            result *= i;
+            i++;
+        }
+        
+        return result;
     }
     
-    public static void Example_4(){
+    public static void FindIterationCount(int limit){
+       int iterationCount = 1;
+       int result = iterationCount;
        
+       while(result + iterationCount < limit && iterationCount< 100){
+           iterationCount++;
+           result += iterationCount;
+       }
+       
+        System.out.println("Loop lmit exceed at " + iterationCount + "th iteration and result is: " + result);
     }
     
     public static void Example_5(){
